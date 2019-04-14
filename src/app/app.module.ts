35a2ba2a738be6c86580179fbfe376fccab8696e
terrichoe <choe1886@gmail.com>
2019-04-13 23:25:58 -0400

    added error page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { LoginComponent } from "./login/login.component";
+import { ErrorComponent } from "./error/error.component";
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent],
+  declarations: [AppComponent, LoginComponent, ErrorComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -8,11 +8,17 @@ import { Component, OnInit } from "@angular/core";
 export class LoginComponent implements OnInit {
   username = "terri";
   password = "terri";
+  invaildLogin = false;
+  errorMessage = "invaild Credentials";
+
   constructor() {}
 
   ngOnInit() {}
 
   handleLogin = () => {
     console.log(`username: ${this.username} password: ${this.password}`);
+    this.invaildLogin = this.username !== "terri" || this.password !== "123";
+    if (!this.invaildLogin) {
+    }
   };
 }
