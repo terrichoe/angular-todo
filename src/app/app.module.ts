ab779753ed9dd3a915e7f7335595f5afc6c561fe
terrichoe <choe1886@gmail.com>
2019-04-16 03:12:04 -0400

    added menu and footer components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,21 @@ import { AppComponent } from "./app.component";
 import { LoginComponent } from "./login/login.component";
 import { ErrorComponent } from "./error/error.component";
 import { AppRoutingModule } from "./app-routing.module";
-import { WelcomeComponent } from './welcome/welcome.component';
-import { ListTodosComponent } from './list-todos/list-todos.component';
+import { WelcomeComponent } from "./welcome/welcome.component";
+import { ListTodosComponent } from "./list-todos/list-todos.component";
+import { MenuComponent } from "./menu/menu.component";
+import { FooterComponent } from "./footer/footer.component";
+
 @NgModule({
-  declarations: [AppComponent, LoginComponent, ErrorComponent, WelcomeComponent, ListTodosComponent],
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    ErrorComponent,
+    WelcomeComponent,
+    ListTodosComponent,
+    MenuComponent,
+    FooterComponent
+  ],
   imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
